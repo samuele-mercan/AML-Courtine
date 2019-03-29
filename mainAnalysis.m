2f8f68cba6ba736ff4d144af995d2c9fc0dd8a03
mercan <samuele.mercan@epfl.ch>
2019-03-29 12:18:17 +0100

    Set up of variable names to load data

--- mainAnalysis.m
+++ mainAnalysis.m
@@ -6,21 +6,21 @@ function main()
     %======================================================================
     % Data with no Crutches 
     
-    datasetName1 = 'Exercise_Material_SCI_Subject/FLOAT_NO_CRUTCHES/MAT/FLOAT_NO_CRUTCHES.mat';
+    datasetName1 = 'Exercise - Material - SCI Subject/FLOAT_NO_CRUTCHES/MAT/FLOAT_NO_CRUTCHES.mat';
     load(datasetName1); % load data structure   
     
-    noCrutches_03 = readtable('Exercise_Material_SCI_Subject/FLOAT_NO_CRUTCHES/GAIT_FILES/SCI_HCU_20150505_04OVGb_45BWS_vFWD_noAD_03_GAIT.csv');
-    noCrutches_04 = readtable('Exercise_Material_SCI_Subject/FLOAT_NO_CRUTCHES/GAIT_FILES/SCI_HCU_20150505_04OVGb_45BWS_vFWD_noAD_04_GAIT.csv');
-    noCrutches_05 = readtable('Exercise_Material_SCI_Subject/FLOAT_NO_CRUTCHES/GAIT_FILES/SCI_HCU_20150505_04OVGb_45BWS_vFWD_noAD_05_GAIT.csv');
+    noCrutches_03 = readtable('Exercise - Material - SCI Subject/FLOAT_NO_CRUTCHES/GAIT FILES/SCI_HCU_20150505_04OVGb_45BWS_vFWD_noAD_03_GAIT.csv');
+    noCrutches_04 = readtable('Exercise - Material - SCI Subject/FLOAT_NO_CRUTCHES/GAIT FILES/SCI_HCU_20150505_04OVGb_45BWS_vFWD_noAD_04_GAIT.csv');
+    noCrutches_05 = readtable('Exercise - Material - SCI Subject/FLOAT_NO_CRUTCHES/GAIT FILES/SCI_HCU_20150505_04OVGb_45BWS_vFWD_noAD_05_GAIT.csv');
     
     % Data with Crutches
     
-    datasetName2 = 'Exercise_Material_SCI_Subject/NO_FLOAT_CRUTCHES/MAT/NO_FLOAT_CRUTCHES.mat';
+    datasetName2 = 'Exercise - Material - SCI Subject/NO_FLOAT_CRUTCHES/MAT/NO_FLOAT_CRUTCHES.mat';
     load(datasetName2); % load data structure   
     
-    Crutches_01 = readtable('Exercise_Material_SCI_Subject/NO_FLOAT_CRUTCHES/GAIT_FILES/SCI_HCU_20150505_02OVGa_AD_01_GAIT.csv');
-    Crutches_02 = readtable('Exercise_Material_SCI_Subject/NO_FLOAT_CRUTCHES/GAIT_FILES/SCI_HCU_20150505_02OVGa_AD_02_GAIT.csv');
-    Crutches_03 = readtable('Exercise_Material_SCI_Subject/NO_FLOAT_CRUTCHES/GAIT_FILES/SCI_HCU_20150505_02OVGa_AD_03_GAIT.csv');
+    Crutches_01 = readtable('Exercise - Material - SCI Subject/NO_FLOAT_CRUTCHES/GAIT FILES/SCI_HCU_20150505_02OVGa_AD_01_GAIT.csv');
+    Crutches_02 = readtable('Exercise - Material - SCI Subject/NO_FLOAT_CRUTCHES/GAIT FILES/SCI_HCU_20150505_02OVGa_AD_02_GAIT.csv');
+    Crutches_03 = readtable('Exercise - Material - SCI Subject/NO_FLOAT_CRUTCHES/GAIT FILES/SCI_HCU_20150505_02OVGa_AD_03_GAIT.csv');
     
     
     % Create the ground truth based on the csv features 
