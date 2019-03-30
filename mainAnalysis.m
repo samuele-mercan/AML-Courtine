89b8173d9c82d203aae5d60da18f09bd76eb5cd6
lbruhin <lena.bruhin@epfl.com>
2019-03-30 10:58:04 +0100

    Update mainAnalysis.m
    plotted csv file data to look at data and events for segmentation

--- mainAnalysis.m
+++ mainAnalysis.m
@@ -25,7 +25,37 @@ function main()
     
     % Create the ground truth based on the csv features 
     
-%% SEGMENTATION USING CSV FILE      
+%% SEGMENTATION USING CSV FILE     
+    L_ANK_02 = NO_FLOAT_CRUTCHES.T_02.Raw.Kin.LANK;
+    
+    % create the time vector for plotting
+    time_start = 0;
+    time_end = (length(L_ANK_02)-1)/NO_FLOAT_CRUTCHES.T_02.fsKIN;
+    time_step = 1/NO_FLOAT_CRUTCHES.T_02.fsKIN;
+    time = time_start:time_step:time_end;
+    A = table2array(Crutches_02(1:9,4));
+    B = round(A*NO_FLOAT_CRUTCHES.T_02.fsKIN,0);
+    C = table2array(Crutches_02(10:17,4));
+    D = round(C*NO_FLOAT_CRUTCHES.T_02.fsKIN,0);
+    
+    figure()
+    plot(time, L_ANK_02(:,1))
+    hold on
+    scatter(A, L_ANK_02(B,1));
+    scatter(C, L_ANK_02(D,1));
+    title("left ankle")
+    legend('ankle kinematics','foot strike', 'foot off')
+    
+    figure()
+    plot(time, NO_FLOAT_CRUTCHES.T_02.Raw.Kin.LTOE(:,1))
+    hold on
+    scatter(A, NO_FLOAT_CRUTCHES.T_02.Raw.Kin.LTOE(B,1));
+    scatter(C, NO_FLOAT_CRUTCHES.T_02.Raw.Kin.LTOE(D,1));
+    legend('foot kinematics','foot strike', 'foot off')
+    title("left toe")
+    
+    
+
 %%    
     %======================================================================
     % Load Data for Healthy Patient
