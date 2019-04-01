6c76c2545b9540bdbdf89aa27b33f25930cacfdc
lbruhin <lena.bruhin@epfl.com>
2019-04-01 14:22:21 +0200

    adjusted filenumbers of csv

--- mainAnalysis.m
+++ mainAnalysis.m
@@ -9,9 +9,9 @@ function main()
     datasetName1 = 'Exercise - Material - SCI Subject/FLOAT_NO_CRUTCHES/MAT/FLOAT_NO_CRUTCHES.mat';
     load(datasetName1); % load data structure   
     
-    noCrutches_03 = readtable('Exercise - Material - SCI Subject/FLOAT_NO_CRUTCHES/GAIT FILES/SCI_HCU_20150505_04OVGb_45BWS_vFWD_noAD_03_GAIT.csv');
-    noCrutches_04 = readtable('Exercise - Material - SCI Subject/FLOAT_NO_CRUTCHES/GAIT FILES/SCI_HCU_20150505_04OVGb_45BWS_vFWD_noAD_04_GAIT.csv');
-    noCrutches_05 = readtable('Exercise - Material - SCI Subject/FLOAT_NO_CRUTCHES/GAIT FILES/SCI_HCU_20150505_04OVGb_45BWS_vFWD_noAD_05_GAIT.csv');
+    noCrutches_01 = readtable('Exercise - Material - SCI Subject/FLOAT_NO_CRUTCHES/GAIT FILES/SCI_HCU_20150505_04OVGb_45BWS_vFWD_noAD_03_GAIT.csv');
+    noCrutches_02 = readtable('Exercise - Material - SCI Subject/FLOAT_NO_CRUTCHES/GAIT FILES/SCI_HCU_20150505_04OVGb_45BWS_vFWD_noAD_04_GAIT.csv');
+    noCrutches_03 = readtable('Exercise - Material - SCI Subject/FLOAT_NO_CRUTCHES/GAIT FILES/SCI_HCU_20150505_04OVGb_45BWS_vFWD_noAD_05_GAIT.csv');
     
     % Data with Crutches
     
@@ -26,33 +26,33 @@ function main()
     % Create the ground truth based on the csv features 
     
 %% SEGMENTATION USING CSV FILE     
-    L_ANK_02 = NO_FLOAT_CRUTCHES.T_02.Raw.Kin.LANK;
+    L_ANK_01 = FLOAT_NO_CRUTCHES.T_01.Raw.Kin.LANK(:,1);
     
     % create the time vector for plotting
     time_start = 0;
-    time_end = (length(L_ANK_02)-1)/NO_FLOAT_CRUTCHES.T_02.fsKIN;
-    time_step = 1/NO_FLOAT_CRUTCHES.T_02.fsKIN;
+    time_end = (length(L_ANK_01)-1)/FLOAT_NO_CRUTCHES.T_01.fsKIN;
+    time_step = 1/FLOAT_NO_CRUTCHES.T_01.fsKIN;
     time = time_start:time_step:time_end;
-    A = table2array(Crutches_02(1:9,4));
-    B = round(A*NO_FLOAT_CRUTCHES.T_02.fsKIN,0);
-    C = table2array(Crutches_02(10:17,4));
-    D = round(C*NO_FLOAT_CRUTCHES.T_02.fsKIN,0);
+    A = table2array(noCrutches_01(1:9,4));
+    B = round(A*FLOAT_NO_CRUTCHES.T_01.fsKIN,0);
+    C = table2array(noCrutches_01(10:17,4));
+    D = round(C*FLOAT_NO_CRUTCHES.T_01.fsKIN,0);
+    
+%     [pks,locs]=findpeaks (L_ANK_01(:,1),'MinPeakDistance',60);
     
     figure()
-    plot(time, L_ANK_02(:,1))
+    plot(time, L_ANK_01)
     hold on
-    scatter(A, L_ANK_02(B,1));
-    scatter(C, L_ANK_02(D,1));
-    title("left ankle")
-    legend('ankle kinematics','foot strike', 'foot off')
+    plot(time, FLOAT_NO_CRUTCHES.T_01.Raw.Kin.LTOE(:,1))
+    plot(time, FLOAT_NO_CRUTCHES.T_01.Raw.Kin.LKNE(:,1))
+
+    scatter(A, L_ANK_01(B));
+    scatter(C, L_ANK_01(D));
+%     scatter(time(locs), pks)
     
-    figure()
-    plot(time, NO_FLOAT_CRUTCHES.T_02.Raw.Kin.LTOE(:,1))
-    hold on
-    scatter(A, NO_FLOAT_CRUTCHES.T_02.Raw.Kin.LTOE(B,1));
-    scatter(C, NO_FLOAT_CRUTCHES.T_02.Raw.Kin.LTOE(D,1));
-    legend('foot kinematics','foot strike', 'foot off')
-    title("left toe")
+   legend('ankle kinematics','Toe kinematics','Knee','foot strike', 'foot off')
+%     legend('ankle kinematics','Toe kinematics','Knee','peaks')
+%     title("left ankle")
     
     
 
