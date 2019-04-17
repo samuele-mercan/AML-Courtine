a312b58172c0176ea94d7f5ecf2503fbe3b5f75e
Ludovica Romanin <ludovica.romanin@epfl.ch>
2019-04-17 21:28:28 +0200

    ADD ground truth data for Healthy Subjects

--- groundTruthTool.m
+++ groundTruthTool.m
@@ -1,11 +1,5 @@
 
-function [data_01_FS_left, data_01_FS_right, ...
-    data_01_FO_left, data_01_FO_right, ...
-    data_02_FS_left, data_02_FS_right, ...
-    data_02_FO_left, data_02_FO_right, ...
-    data_03_FS_left, data_03_FS_right, ...
-    data_03_FO_left, data_03_FO_right] = groundTruthTool(dataName)
-
+function groundTruthTool(dataName, fileName)
 
 % this tool plots each raw signal for the kinematics with a cursor 
 % by looking at the signal, the locations of the gait events are found
@@ -18,10 +12,11 @@ figure()
 hold on
 title(strcat(dataName, "_T01_left"))
 plot(data.(fields{1}).T_01.Raw.Kin.LTOE(:,3));
+fsKin = data.(fields{1}).T_01.fsKin;
 [x, y] = ginput;
-data_01_FS_left = int16(x);
+T01_FS_left = x/fsKin;
 [x, y] = ginput;
-data_01_FO_left = int16(x);
+T01_FO_left = x/fsKin;
 
 % figure()
 % hold on
@@ -33,45 +28,66 @@ figure()
 hold on
 title(strcat(dataName, "_T01_right"))
 plot(data.(fields{1}).T_01.Raw.Kin.RTOE(:,3));
+
 [x, y] = ginput;
-data_01_FS_right = int16(x);
+T01_FS_right = x/fsKin;
 [x, y] = ginput;
-data_01_FO_right = int16(x);
+T01_FO_right = x/fsKin;
 
 figure()
 hold on
 title(strcat(dataName, "_T02_left"))
 plot(data.(fields{1}).T_02.Raw.Kin.LTOE(:,3));
+fsKin = data.(fields{1}).T_02.fsKin;
 [x, y] = ginput;
-data_02_FS_left = int16(x);
+T02_FS_left = x/fsKin;
 [x, y] = ginput;
-data_02_FO_left = int16(x);
+T02_FO_left = x/fsKin;
 
 figure()
 hold on
 title(strcat(dataName, "_T02_right"))
 plot(data.(fields{1}).T_02.Raw.Kin.RTOE(:,3));
 [x, y] = ginput;
-data_02_FS_right = int16(x);
+T02_FS_right = x/fsKin;
 [x, y] = ginput;
-data_02_FO_right = int16(x);
+T02_FO_right = x/fsKin;
 
 figure()
 hold on
 title(strcat(dataName, "_T03_left"))
 plot(data.(fields{1}).T_03.Raw.Kin.LTOE(:,3));
+fsKin = data.(fields{1}).T_03.fsKin;
 [x, y] = ginput;
-data_03_FS_left = int16(x);
+T03_FS_left = x/fsKin;
 [x, y] = ginput;
-data_03_FO_left = int16(x);
+T03_FO_left = x/fsKin;
 
 figure()
 hold on
 title(strcat(dataName, "_T03_right"))
 plot(data.(fields{1}).T_03.Raw.Kin.RTOE(:,3));
 [x, y] = ginput;
-data_03_FS_right = int16(x);
+T03_FS_right = x/fsKin;
 [x, y] = ginput;
-data_03_FO_right = int16(x);
+T03_FO_right = x/fsKin;
 
+gaitEvents = struct();
+
+gaitEvents.T01_FS_left = T01_FS_left;
+gaitEvents.T01_FO_left = T01_FO_left;
+gaitEvents.T01_FS_right = T01_FS_right;
+gaitEvents.T01_FO_right = T01_FO_right;
+gaitEvents.T02_FS_left = T02_FS_left;
+gaitEvents.T02_FO_left = T02_FO_left;
+gaitEvents.T02_FS_right = T02_FS_right;
+gaitEvents.T02_FO_right = T02_FO_right;
+gaitEvents.T03_FS_left = T03_FS_left;
+gaitEvents.T03_FO_left = T03_FO_left;
+gaitEvents.T03_FS_right = T03_FS_right;
+gaitEvents.T03_FO_right = T03_FO_right;
+
+save(fileName, 'gaitEvents');
+
+end
 
