e1b432d51cd6badba01b5b5bc3c547ab430b80f4
lbruhin <lena.bruhin@epfl.ch>
2019-04-24 08:12:22 +0200

    smaller changes in PCA code

--- mainPCA.m
+++ mainPCA.m
@@ -27,21 +27,20 @@ features_pc1 = round(10*(explained(1)/percentage_3_PCs),0);
 features_pc2 = round(10*(explained(2)/percentage_3_PCs),0);
 features_pc3 = round(10*(explained(3)/percentage_3_PCs),0);
 
-top10_loading = [];
+top10_sorted = [];
 top10_features = [];
 
 for i=1:3
-    [loading, feature_index] = sort(coeff(:,i),'descend')
-    top10_loading = [top10_loading, loading];
+    [sorted, feature_index] = sort(coeff(:,i),'descend')
+    top10_sorted = [top10_sorted, sorted];
     top10_features = [top10_features, feature_index];
 end
 
 
 
-top10_ = [top10_loading(1:features_pc1,1);...
-    top10_loading(1:features_pc2,2); top10_loading(1:features_pc3,3)];
-%add code for the labels of those features!!
-top10_labels = [top10_features(1:features_pc1,1);...
+top10_ = [top10_sorted(1:features_pc1,1);...
+    top10_sorted(1:features_pc2,2); top10_sorted(1:features_pc3,3)];
+top10_labels_right = [top10_features(1:features_pc1,1);...
     top10_features(1:features_pc2,2); top10_features(1:features_pc3,3)];
 
 HealthyNoFloat = 1:101;
@@ -74,21 +73,11 @@ legend('Cumulative variance', '95% threshold')
 figure('Name', 'Top10 features contributing to first 3 PCs')
 bar(1:10, top10_)
 hold on
-ylabel('Loading of feature')
+ylabel('Contribution of feature')
 xticks(1:10)
 xtickangle(45)
-xticklabels(variableNames(top10_labels))
+xticklabels(variableNames(top10_labels_right))
 
-
-% % kmeans into 4 clusters
-% idx4 = kmeans(score(1:2,:),4);
-% T4 = ones(1, length(idx4))
-% [Acc4,rand_index4,match4]=AccMeasure(T4,idx4)
-
-% % kmeans into 8 clusters
-% idx8 = kmeans(score(1:2,:),8);
-% T8 = ones(1, length(idx8))
-% [Acc8,rand_index8,match8]=AccMeasure(T8,idx8)
 
 %anova1 to compare if a feature is significantly different between SCI and
 %healthy
@@ -96,6 +85,32 @@ xticklabels(variableNames(top10_labels))
 %data for SCI or healthy, then you enter this matrix into anova1() and it
 %will give you a p-value
 %example: p =  anova1(data_for_swing_time)
+a = Param_Healthy_NoFloat_Right(:,top10_labels_right(1));
+b = Param_SCI_NoFloat_Right(:,top10_labels_right(1));
+n = max(numel(a), numel(b));
+a(end+1:n)=NaN;
+b(end+1:n)=NaN;
+p1 = anova1([a,b]);
+
+figure(37)
+bar(1:2, [nanmean(Param_Healthy_NoFloat_Right(:,top10_labels_right(1))),...
+    nanmean(Param_SCI_NoFloat_Right(:,top10_labels_right(1)))])
+hold on
+ylabel(variableNames(top10_labels_right(1)))
+xticks(1:2)
+xtickangle(45)
+xticklabels({'Healthy No Float', 'SCI No Float'})
+
+
+a = Param_Healthy_NoFloat_Right(:,top10_labels_right(2));
+b = Param_SCI_NoFloat_Right(:,top10_labels_right(2));
+n = max(numel(a), numel(b));
+a(end+1:n)=NaN;
+b(end+1:n)=NaN;
+p2 = anova1([a,b]);
+
+
+
 
 %% PCA: segmentation with respect to left foot
 
@@ -129,8 +144,11 @@ for i=1:3
     top10_features = [top10_features, feature_index];
 end
 
-top10_features = [top10_loading(1:features_pc1,1);...
+top10_ = [top10_loading(1:features_pc1,1);...
     top10_loading(1:features_pc2,2); top10_loading(1:features_pc3,3)];
+top10_labels_left = [top10_features(1:features_pc1,1);...
+    top10_features(1:features_pc2,2); top10_features(1:features_pc3,3)];
+
 
 %indeces of each "submatrix" that is put into the PCA function
 HealthyNoFloat = 1:100;
@@ -161,8 +179,12 @@ ylabel('Cumulative variance')
 legend('Cumulative variance', '95% threshold')
 
 figure('Name', 'Top10 features contributing to first 3 PCs')
-bar(1:10, top10_features)
+bar(1:10, top10_)
+hold on
 ylabel('Loading of feature')
+xticks(1:10)
+xtickangle(45)
+xticklabels(variableNames(top10_labels_left))
 
 % % kmeans into 4 clusters
 % idx4 = kmeans(score(1:2,:),4);
