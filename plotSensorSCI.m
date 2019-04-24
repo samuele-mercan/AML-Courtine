ac0fed59145145df99c988882bfea8cdc9cb9736
mercan <samuele.mercan@epfl.ch>
2019-04-24 02:08:27 +0200

    Added function to plot SCI patient data

--- plotSensorSCI.m
+++ plotSensorSCI.m
@@ -1,23 +1,20 @@
-function [] = PlotSensorHealthy(data, time, subject, sensorType)
+function [] = plotSensorSCI(data, time, sensorType)
 % Plot the movement of the sensor. On the same plot we see all the axis
 % movement.
 %   Takes the segmented input data and connects them in a whole plot in
 %   order to have a continuos visualization of the movement of the sensor
 %   in time.
-%   data: segmented structure derived from: HealthySubjectSegmentation.
+%   data: segmented structure derived from: SCIsegmentation.
 %   time: boolean, if time plot in seconds, else in frequency.
-%   subject: string, which healthy subject we are studying.
 %   sensorType: string, which sensor we want to plot
-%   FLOATorNOT: string, is it FLOAT or NOFLOAT depending on what we want to
-%   plot.
 
 frequency = 100;
 
-gaitCycles = fieldnames(data.(subject).(FLOATorNOT));
+gaitCycles = fieldnames(data);
 plotData = [];
 for i = 1:numel(gaitCycles)
     GC = char(gaitCycles(i));
-    plotData = [plotData; data.(subject).(FLOATorNOT).(GC).Kin.(sensorType)];
+    plotData = [plotData; data.(GC).Kin.(sensorType)];
 end
 sizeData = size(plotData(:,1));
 samplePoints = linspace (0,sizeData(1)-1,sizeData(1));
@@ -28,6 +25,5 @@ plot(samplePoints, plotData(:,1),  '-r',...
 samplePoints,plotData(:,2), '--b',...
 samplePoints,plotData(:,3), '.-c');  
 legend('x-coordinate', 'y-coordinate', 'z-coordinate');
-
 end
 
