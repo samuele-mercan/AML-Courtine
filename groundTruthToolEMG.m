1d0a6cf125310e6f7db9c5bbef2cc7d3e9448898
alicebizeul <alice.bizeul@epfl.ch>
2019-04-26 10:45:47 +0200

    GroundTruchMakerEMG

--- groundTruthToolEMG.m
+++ groundTruthToolEMG.m
@@ -1,19 +1,18 @@
 
-function groundTruthToolEMG(dataName, fileName)
+[GaitCyclesFloat1,GaitCyclesNoFloat1] = ...
+    HealthySubjectSegmentation('Healthy Recordings/Subject1_2018/FLOAT/S1_FLOAT.mat', ...
+    'Healthy Recordings/Subject1_2018/NO_FLOAT/S1_NO_FLOAT.mat',...
+    'HealthyGaitEvents/2018_S1_FLOAT.mat', 'HealthyGaitEvents/2018_S1_NO_FLOAT.mat',leftFoot);
 
-% this tool plots each raw signal for the kinematics with a cursor 
-% by looking at the signal, the locations of the gait events are found
-% for each signal we find the foot strikes and the foot off 
-
-data = load(dataName);
+data = GaitCyclesFloat1;
 fields = fieldnames(data);
 
 for i = 1:numel(fields)    
-    fields2 = fieldnames(data.(fields{i}));
+    fields2 = fieldnames(data.(fields{i}).EMG);
     for j = 1:4
         figure()
         hold on
-        title(strcat(dataName, fields2{j}))
+        title(strcat(fields2{j}))
         plot(data.(fields{i}).EMG.(fields2{j}));
         [Onsets.(fields{i}).EMG.(fields2{j}), ~] = ginput;
         [Offsets.(fields{i}).EMG.(fields2{j}), ~] = ginput;
@@ -22,7 +21,7 @@ end
 
 save([pwd,'/GroundTruthBursts/2018_S1_FLOAT.mat'], 'Onsets','Offsets');
 
-end
+
 
 
 
