f14521a811bfb1990f9acc9a59321ac7b66d6086
laurefont <laure.font@epfl.ch>
2019-04-26 15:37:01 +0200

    Ground truth EMG subject healthy 1-6

---
+++ GroundTruthToolEMGLaure.m
@@ -0,0 +1,21 @@
+function GroundTruthToolEMGLaure(data,fileName)
+
+    fields = fieldnames(data);
+
+    for i = 1:numel(fields)    
+        fields2 = fieldnames(data.(fields{i}).EMG);
+        for j = 1:4
+            figure()
+            hold on
+            title(strcat(fields2{j}))
+            plot(data.(fields{i}).EMG.(fields2{j}));
+            [Onsets.(fields{i}).EMG.(fields2{j}), ~] = ginput;
+            [Offsets.(fields{i}).EMG.(fields2{j}), ~] = ginput;
+        end
+    end
+
+    save([pwd,fileName], 'Onsets','Offsets');
+    close all
+
+end
+
