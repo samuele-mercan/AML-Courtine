84c9e082f241b5ef2f39f48fc0034cb53e019638
alicebizeul <alice.bizeul@epfl.ch>
2019-04-26 10:35:34 +0200

    Ground Truth Maker EMG

--- groundTruthToolEMG.m
+++ groundTruthToolEMG.m
@@ -8,88 +8,19 @@ function groundTruthToolEMG(dataName, fileName)
 data = load(dataName);
 fields = fieldnames(data);
 
-figure()
-hold on
-title(strcat(dataName, "_T01_left-LMG"))
-plot(data.(fields{1}).T_01.Raw.EMG.LMG);
-fsEMG = data.(fields{1}).T_01.fsEMG;
-[x, y] = ginput;
-T01_Onset_LMG_left = x/fsEMG;
-[x, y] = ginput;
-T01_FO_left = x/fsEMG;
+for i = 1:numel(fields)    
+    fields2 = fieldnames(data.(fields{i}));
+    for j = 1:4
+        figure()
+        hold on
+        title(strcat(dataName, fields2{j}))
+        plot(data.(fields{i}).EMG.(fields2{j}));
+        [Onsets.(fields{i}).EMG.(fields2{j}), ~] = ginput;
+        [Offsets.(fields{i}).EMG.(fields2{j}), ~] = ginput;
+    end
+end
 
-
-
-% figure()
-% hold on
-% plot(data.(fields{1}).T_01.Raw.Kin.LTOE(:,3));
-% scatter(data_01_FS_left,data.(fields{1}).T_01.Raw.Kin.LTOE(data_01_FS_left,3), 'b');
-% scatter(data_01_FO_left,data.(fields{1}).T_01.Raw.Kin.LTOE(data_01_FO_left,3), 'r');
-
-figure()
-hold on
-title(strcat(dataName, "_T01_right"))
-plot(data.(fields{1}).T_01.Raw.EMG.RMG);
-
-[x, y] = ginput;
-T01_FS_right = x/fsEMG;
-[x, y] = ginput;
-T01_FO_right = x/fsEMG;
-
-figure()
-hold on
-title(strcat(dataName, "_T02_left"))
-plot(data.(fields{1}).T_02.Raw.EMG.LTA);
-fsKin = data.(fields{1}).T_02.fsEMG;
-[x, y] = ginput;
-T02_FS_left = x/fsKin;
-[x, y] = ginput;
-T02_FO_left = x/fsKin;
-
-figure()
-hold on
-title(strcat(dataName, "_T02_right"))
-plot(data.(fields{1}).T_02.Raw.Kin.RTOE(:,3));
-[x, y] = ginput;
-T02_FS_right = x/fsKin;
-[x, y] = ginput;
-T02_FO_right = x/fsKin;
-
-figure()
-hold on
-title(strcat(dataName, "_T03_left"))
-plot(data.(fields{1}).T_03.Raw.Kin.LTOE(:,3));
-fsKin = data.(fields{1}).T_03.fsKin;
-[x, y] = ginput;
-T03_FS_left = x/fsKin;
-[x, y] = ginput;
-T03_FO_left = x/fsKin;
-
-figure()
-hold on
-title(strcat(dataName, "_T03_right"))
-plot(data.(fields{1}).T_03.Raw.Kin.RTOE(:,3));
-[x, y] = ginput;
-T03_FS_right = x/fsKin;
-[x, y] = ginput;
-T03_FO_right = x/fsKin;
-
-gaitEvents = struct();
-
-gaitEvents.T01_FS_left = T01_FS_left;
-gaitEvents.T01_FO_left = T01_FO_left;
-gaitEvents.T01_FS_right = T01_FS_right;
-gaitEvents.T01_FO_right = T01_FO_right;
-gaitEvents.T02_FS_left = T02_FS_left;
-gaitEvents.T02_FO_left = T02_FO_left;
-gaitEvents.T02_FS_right = T02_FS_right;
-gaitEvents.T02_FO_right = T02_FO_right;
-gaitEvents.T03_FS_left = T03_FS_left;
-gaitEvents.T03_FO_left = T03_FO_left;
-gaitEvents.T03_FS_right = T03_FS_right;
-gaitEvents.T03_FO_right = T03_FO_right;
-
-save(fileName, 'gaitEvents');
+save([pwd,'/GroundTruthBursts/2018_S1_FLOAT.mat'], 'Onsets','Offsets');
 
 end
 
