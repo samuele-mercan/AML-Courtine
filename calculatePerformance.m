2101fefe35c06feee7d2e5140c7e19ae987569d2
LudovicaR <ludovica.romanin@epfl.ch>
2019-04-29 14:03:35 +0200

    UPDATE calculatePerformance, plot with and without outliers

--- calculatePerformance.m
+++ calculatePerformance.m
@@ -4,6 +4,21 @@ function calculatePerformance()
     detectionErrorFloat = algorithmPerformance('DetectedHealthyGaitEvents/*FLOAT*', 'HealthyGaitEvents/*FLOAT*');
     detectionErrorNoFloat = algorithmPerformance('DetectedHealthyGaitEvents/*NO_FLOAT*', 'HealthyGaitEvents/*NO_FLOAT*');
 
+    
+    
+    %without outliers
+    figure()
+    hold on
+    title('Error in the gait events detection for the FLOAT and NO FLOAT data')
+    ylabel('Error in the detection of the gait events')
+    group = [ ones(size(detectionErrorFloat')); 2 * ones(size(detectionErrorNoFloat'))];
+    bPlot = boxplot([detectionErrorFloat';detectionErrorNoFloat'], group, 'symbol','');
+    set(gca, 'XTickLabel', {'Float', 'No Float'})
+    ylim([-0.1 1.4]);
+    outliers = findobj(bPlot,'Tag','Outliers');
+    yy = get(outliers,'YData');
+    
+    %with outliers
     figure()
     hold on
     title('Error in the gait events detection for the FLOAT and NO FLOAT data')
@@ -11,7 +26,6 @@ function calculatePerformance()
     group = [ ones(size(detectionErrorFloat')); 2 * ones(size(detectionErrorNoFloat'))];
     bPlot = boxplot([detectionErrorFloat';detectionErrorNoFloat'], group);
     set(gca, 'XTickLabel', {'Float', 'No Float'})
-    
     outliers = findobj(bPlot,'Tag','Outliers');
     yy = get(outliers,'YData');
     
