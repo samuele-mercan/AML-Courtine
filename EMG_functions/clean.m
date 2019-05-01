7be125442a977e08e22d2564204642b5b808e4c0
alicebizeul <alice.bizeul@epfl.ch>
2019-05-01 15:57:57 +0200

    Clening EMG files

--- EMG_functions/clean.m
+++ EMG_functions/clean.m
@@ -1,23 +1,15 @@
 function [onset,offset] = clean(onset,offset,window)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+%
+
 threshold = window;
-threshold2 = window;
 
 % selection based on interval between bursts
 if length(onset) >= 2
     interval = onset(2:end) - offset(1:end-1);
-    index = interval > threshold2;
+    index = interval > threshold;
     onset = onset(logical([1 index]));
     offset = offset(logical([index 1]));
 end
-
-% if length(onset) >= 2
-%     duration = offset - onset;
-%     [~,index] = max(duration);
-%     onset = onset(index);
-%     offset = offset(index);
-% end
 
 if isempty(onset)
     onset = NaN;
